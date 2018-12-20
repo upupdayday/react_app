68db72b8c8eb50e87869a84e34b3e193de17e8af
upupdayday <sunhanqing1987@163.com>
2018-12-21 05:39:15 +0800

    1.修改package.json指定主页;2.去掉ignore内的build;

---
+++ build/precache-manifest.36840962b87100d48973149e9112885f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_app/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2584550873e91f3e67e4",
+    "url": "/react_app/build/static/js/runtime~main.25845508.js"
+  },
+  {
+    "revision": "930a56fe24e141b1a55a",
+    "url": "/react_app/build/static/js/main.930a56fe.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react_app/build/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "930a56fe24e141b1a55a",
+    "url": "/react_app/build/static/css/main.a3c9be9a.chunk.css"
+  },
+  {
+    "revision": "93a1d81f95dc96c889d1ff7c8b9da411",
+    "url": "/react_app/build/index.html"
+  }
+];
