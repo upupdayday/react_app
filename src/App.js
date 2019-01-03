d62d9ebcadb9dd00a63595523eaea87baefba701
upupdayday <sunhanqing1987@163.com>
2019-01-04 06:49:24 +0800

    实现todoList基本框架

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTodo: 'test',
+            todoList: [
+                {id:1, title:'第一个待办'}
+            ],
+        };
+    }
+    render() {
+        let todos = this.state.todoList.map((item,index)=>{
+            return <li>{item.title}</li>
+        })
+        return (
+          <div className="App">
+              <h1>我的待办</h1>
+              <div className="inputWrapper">
+                  {/*注意 value= 后面不要加引号，加了你试下，会错*/}
+                  <input type="text" value={this.state.newTodo}/>
+              </div>
+              <ol>
+                  {todos}
+              </ol>
+          </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,62 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import AddByFunction from'./counter'
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(
-//     <h1>Hello, world!</h1>,
-//     document.getElementById('root')
-// );
-//
-function plusone(cc, props){
-    console.log(cc);
-    return{count: cc.count + 1};
-}
-
-class AddByFunction extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {count: 0};
-        this.increment = this.increment.bind(this);
-    }
-
-    increment() {
-        this.setState(plusone);
-        this.setState(plusone);
-
-    }
-
-    render(){
-        console.log('cc');
-        return (
-            <div>
-                <button onClick={this.increment}>AddByFunction</button>
-                <h2>Result:{this.state.count}</h2>
-            </div>
-        );
-    }
-}
 ReactDOM.render(
-    <AddByFunction />,
+    <App />,
     document.getElementById('root')
 );
-
-// ReactDOM.render(
-//     /*<h1>Hello, world!</h1>,*/
-//     <AddByFunction />,
-//     document.getElementById('root')
-// );
-
-// ReactDOM.render(
-//     /*<h1>Hello, world!</h1>,*/
-//     <Counter />,
-//     document.getElementById('root')
-// );
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
