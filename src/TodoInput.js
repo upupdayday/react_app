134bf42c3a0eaf56f7fd99c288638125012a2d21
upupdayday <sunhanqing1987@163.com>
2019-01-07 06:06:50 +0800

    this.submit不传参数，笔误

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
 class TodoInput extends Component{
-    submit(e) {
+    submit(e){
         if (e.key === 'Enter') {
             console.log('用户按回车了');
         }
     }
-    
     render(){
         return <input type="text" defaultValue={this.props.content}
-        onKeyPress={this.submit(e)}/>
+        onKeyPress={this.submit}/>
     }
 
 
