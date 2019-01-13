298c43d9c83d5472c57da360b068acf8747eafae
upupdayday <sunhanqing1987@163.com>
2019-01-13 21:51:10 +0800

    实现切换效果

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -17,6 +17,43 @@ class UserDialog extends Component{
     }
 
     render(){
+        let signUpForm = (
+            <form className="signUp"> {/* 注册*/}
+                <div className="row">
+                    <label>用户名</label>
+                    <input type="text"/>
+                </div>
+                <div className="row">
+                    <label>密码</label>
+                    <input type="password"/>
+                </div>
+                <div className="row actions">
+                    <button type="submit">注册</button>
+                </div>
+            </form>
+        );
+
+        let signInForm = (
+            <form className="signIn"> {/* 登录*/}
+                <div className="row">
+                    <label>用户名</label>
+                    <input type="text"/>
+                </div>
+                <div className="row">
+                    <label>密码</label>
+                    <input type="password"/>
+                </div>
+                <div className="row actions">
+                    <button type="submit">登录</button>
+                </div>
+            </form>
+        );
+        
+        const formSelected = this.state.selected;
+
+        let renderForm;
+        renderForm = ('signUp' === formSelected) ? signUpForm : signInForm;
+
         return(
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
@@ -25,32 +62,7 @@ class UserDialog extends Component{
                         <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/>登录</label>
                     </nav>
                     <div className="panes">
-                        <form className="signUp"> {/* 注册*/}
-                            <div className="row">
-                                <label>用户名</label>
-                                <input type="text"/>
-                            </div>
-                            <div className="row">
-                                <label>密码</label>
-                                <input type="password"/>
-                            </div>
-                            <div className="row actions">
-                                <button type="submit">注册</button>
-                            </div>
-                        </form>
-                        <form className="signIn"> {/* 登录*/}
-                            <div className="row">
-                                <label>用户名</label>
-                                <input type="text"/>
-                            </div>
-                            <div className="row">
-                                <label>密码</label>
-                                <input type="password"/>
-                            </div>
-                            <div className="row actions">
-                                <button type="submit">登录</button>
-                            </div>
-                        </form>
+                        {renderForm}
                     </div>
                 </div>
             </div>
