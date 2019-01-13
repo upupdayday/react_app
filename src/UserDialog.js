c988c6b7be769606ece5bd21e369392eb5651b70
upupdayday <sunhanqing1987@163.com>
2019-01-13 22:43:18 +0800

    用户名密码与数据的绑定

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -5,9 +5,15 @@ class UserDialog extends Component{
     constructor(props){
         super(props)
         this.state = {
-            selected: 'signUp'
+            selected: 'signUp',
+            formData:{
+                username:'',
+                password:''
+            }
         }
-        this.switch = this.switch.bind(this);
+        this.switch = this.switch.bind(this)
+        this.changeUsername = this.changeUsername.bind(this);
+        this.changePassword = this.changePassword.bind(this);
     }
 
     switch(e) {
@@ -15,17 +21,43 @@ class UserDialog extends Component{
             selected: e.target.value
         })
     }
+    signUp(e){}
+    signIn(e){}
+
+    changeUsername(e){
+        console.log(this.state)
+        this.setState({
+            formData:{
+                username: e.target.value,
+                password: this.state.formData.password
+            }
+        })
+        console.log(this.state)
+    }
+
+    changePassword(e){
+        console.log(this.state)
+        this.setState({
+            formData:{
+                username:this.state.formData.username,
+                password: e.target.value
+            }
+        })
+        console.log(this.state)
+    }
 
     render(){
         let signUpForm = (
-            <form className="signUp"> {/* 注册*/}
+            <form className="signUp" onSubmit={this.signUp}> {/* 注册*/}
                 <div className="row">
                     <label>用户名</label>
-                    <input type="text"/>
+                    <input type="text" value={this.state.formData.username}
+                    onChange={this.changeUsername}/>
                 </div>
                 <div className="row">
                     <label>密码</label>
-                    <input type="password"/>
+                    <input type="password" value={this.state.formData.password}
+                    onChange={this.changePassword}/>
                 </div>
                 <div className="row actions">
                     <button type="submit">注册</button>
@@ -34,21 +66,23 @@ class UserDialog extends Component{
         );
 
         let signInForm = (
-            <form className="signIn"> {/* 登录*/}
+            <form className="signIn" onSubmit={this.signIn}> {/* 登录*/}
                 <div className="row">
                     <label>用户名</label>
-                    <input type="text"/>
+                    <input type="text" value={this.state.formData.username}
+                    onChange={this.changeUsername}/>
                 </div>
                 <div className="row">
                     <label>密码</label>
-                    <input type="password"/>
+                    <input type="password" value={this.state.formData.password}
+                    onChange={this.changePassword}/>
                 </div>
                 <div className="row actions">
                     <button type="submit">登录</button>
                 </div>
             </form>
         );
-        
+
         const formSelected = this.state.selected;
 
         let renderForm;
@@ -57,9 +91,13 @@ class UserDialog extends Component{
         return(
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
-                    <nav onChange={this.switch}>
-                        <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/>注册</label>
-                        <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/>登录</label>
+                    {/*<nav onChange={this.switch}>*/
+                    /*上面那样写会有warning,底下的input没有onchange*/}
+                    <nav>
+                        <label><input type="radio" value="signUp" onChange={this.switch}
+                                      checked={this.state.selected === 'signUp'}/>注册</label>
+                        <label><input type="radio" value="signIn" onChange={this.switch}
+                                      checked={this.state.selected === 'signIn'}/>登录</label>
                     </nav>
                     <div className="panes">
                         {renderForm}
